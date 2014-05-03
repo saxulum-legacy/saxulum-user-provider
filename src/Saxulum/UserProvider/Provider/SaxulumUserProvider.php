88d536436c3aba9541d9143685b033243ba6f7e4
Dominik Zogg <dominik.zogg@gmail.com>
2014-05-03 11:32:41 +0200

    provider have to work without silex

---
+++ src/Saxulum/UserProvider/Provider/SaxulumUserProvider.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Saxulum\UserProvider\Provider;
+
+use Saxulum\UserProvider\Model\AbstractUser;
+
+class SaxulumUserProvider
+{
+    /**
+     * @param \Pimple $container
+     */
+    public function register(\Pimple $container)
+    {
+        $container['saxulum.userprovider.name'] = 'default';
+        $container['saxulum.userprovider.pattern'] = '/';
+        $container['saxulum.userprovider.loginpath'] = 'login';
+        $container['saxulum.userprovider.checkpath'] = 'login_check';
+        $container['saxulum.userprovider.logoutpath'] = 'logout';
+        $container['saxulum.userprovider.objectmanagerkey'] = 'orm.em';
+        $container['saxulum.userprovider.userclass'] = '';
+        $container['saxulum.userprovider.anonymous'] = true;
+
+        $container['security.firewalls'] = $container->share(function () use ($container) {
+            return array(
+                $container['saxulum.userprovider.name'] => array(
+                    'pattern' => $container['saxulum.userprovider.pattern'],
+                    'form' => array(
+                        'login_path' => $container['saxulum.userprovider.loginpath'],
+                        'check_path' => $container['saxulum.userprovider.checkpath'],
+                    ),
+                    'logout' => array(
+                        'logout_path' => $container['saxulum.userprovider.logoutpath']
+                    ),
+                    'users' => $container->share(function () use ($container) {
+                        return new UserProvider(
+                            $container[$container['saxulum.userprovider.objectmanagerkey']],
+                            $container['saxulum.userprovider.userclass']
+                        );
+                    }),
+                    'anonymous' => $container['saxulum.userprovider.anonymous'],
+                ),
+            );
+        });
+
+        $container['security.access_rules'] = array(
+            array('^/[^/]*/login', 'IS_AUTHENTICATED_ANONYMOUSLY'),
+        );
+
+        $container['security.role_hierarchy'] = array(
+            AbstractUser::ROLE_ADMIN => array(AbstractUser::ROLE_USER),
+        );
+    }
+}
