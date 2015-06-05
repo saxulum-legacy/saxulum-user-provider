fcf10267f460f04b92a33f314f21a8588228cf59
Dominik Zogg <dominik.zogg@gmail.com>
2015-06-05 05:34:03 +0000

    fix deprecated configureOptions

--- src/Saxulum/UserProvider/Form/AbstractUserType.php
+++ src/Saxulum/UserProvider/Form/AbstractUserType.php
@@ -5,7 +5,7 @@ namespace Saxulum\UserProvider\Form;
 use Saxulum\UserProvider\Model\AbstractUser;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 abstract class AbstractUserType extends AbstractType
 {
@@ -37,9 +37,13 @@ abstract class AbstractUserType extends AbstractType
         ;
     }
 
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
     {
-        parent::setDefaultOptions($resolver);
+        parent::configureOptions($resolver);
+
         $resolver->setDefaults(array(
             'data_class' => $this->userClass
         ));
